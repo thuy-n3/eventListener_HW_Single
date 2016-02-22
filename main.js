1e97230e51d3f89dede26961ac82e053e11cfd7b
Thuy Nguyen <Thuy@Thuys-MacBook-Pro.local>
2016-02-22 09:42:58 -0600

    highlighting works need work on 3

--- main.js
+++ main.js
@@ -38,7 +38,41 @@ hidingNavButton.addEventListener("click", navMenuToggle)
 //TASK #1 end-------------------------------------------------//
 
 
-//TASK #2 -------------------------------------------------//
+//TASK #2 ---------------------------------------------------//
+
+//working without the highlighting
+
+// var guestInputEl = document.querySelector('.guestTextBox') //input[type="text"]
+
+// var guestListUl = document.querySelector('.guest-list')
+
+// var addGuest = function(keyEvent){
+
+// 	var guestInputEl = keyEvent.srcElement
+// 		//console.log(guestInputEl) 
+
+// 	if(keyEvent.keyCode === 13){
+// 		var userInput = guestInputEl.value
+// 			console.log(userInput)
+// 		var newLi = document.createElement('li')
+// 			console.log(newLi)
+
+// 		newLi.textContent = userInput
+// 			console.log(newLi.textContent)
+// 		guestListUl.appendChild(newLi)
+		
+// 		guestInputEl.value = ''
+// 	}
+
+// }
+
+// guestInputEl.addEventListener('keydown', addGuest)
+
+
+//-------------------------------------
+
+//hightlight trial 
+
 
 var guestInputEl = document.querySelector('.guestTextBox') //input[type="text"]
 
@@ -46,18 +80,31 @@ var guestListUl = document.querySelector('.guest-list')
 
 var addGuest = function(keyEvent){
 
-	var guestInputEl = keyEvent.srcElement 
+	var guestInputEl = keyEvent.srcElement
+		//console.log(guestInputEl) 
 
 	if(keyEvent.keyCode === 13){
 		var userInput = guestInputEl.value
+			//console.log(userInput)
 		var newLi = document.createElement('li')
+			//console.log(newLi)
+
 		newLi.textContent = userInput
+			//console.log(newLi.textContent)
+
+		newLi.classList.add("guest")	
+
 		guestListUl.appendChild(newLi)
+		
 		guestInputEl.value = ''
 	}
 
 }
 
+guestInputEl.addEventListener('keydown', addGuest)
+
+
+//--------------------------------------------------------
 // var​ lis = document.querySelectorAll('li.product');
 
 // for (var i=0,len=lis.length;i<len;i++){
@@ -68,25 +115,37 @@ var addGuest = function(keyEvent){
 
 	// var highlightLi = document.getElementsByTagName('.guest-List Li')
 
-	// var liBox = []
-	// for (var i=0; i<.length; i++){
-	// 	if()
-	// }
+	// for(var i=0; i<.guestList.length; i++  ){
+	//		if(.guest:nth-child 
+	//}
 
-guestInputEl.addEventListener('keydown', addGuest)
-
+//TASK #2 end-------------------------------------------------//
 
 
+//TASK #3 ---------------------------------------------------//
 
+var guestInput = document.querySelector('.guestInputBonus')
 
+var guestBonusUl = document.querySelector('.guest-ListBonus')
 
+var addBonusGuest = function(keyEvent){
 
+	var bonusGuestEl = keyEvent.srcElement
+		 
 
+	if(keyEvent.keyCode === 13){
+		var guestInputBonus = bonusGuestEl.value
+			
+		var newBonusLi = document.createElement('li')
 
+		newBonusLi.textContent = guestInputBonus
+			
+		guestBonusUl.appendChild(newLi)
+		
+		guestBonusUl.value = ''
+	}
 
+}
 
-
-
-
-
+bonusGuestEl.addEventListener('keydown', addBonusGuest)
 
